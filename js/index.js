7a32a94a6eb104c2cb9b7b16ca13c86ea67681e0
David Sandoval <david.sandoval-g@mail.escuelaing.edu.co>
2022-09-21 15:47:57 -0500

    Añadiendo modificaciones finales calculadora

--- js/index.js
+++ js/index.js
@@ -1,17 +1,14 @@
 const displayValorAnterior = document.getElementById('valor-anterior');
-
 const displayValorActual = document.getElementById('valor-actual');
-
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 
-
 const display = new Display(displayValorAnterior, displayValorActual);
 
-botonesNumeros.array.forEach(boton => {
-    boton.addEventListener('click',()=>display.agregarNumero(boton.innerHTML));
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
 });
 
 botonesOperadores.forEach(boton => {
-    boton.addEventListener('click',()=>display.computar(boton.value));
+    boton.addEventListener('click', () => display.computar(boton.value))
 });
